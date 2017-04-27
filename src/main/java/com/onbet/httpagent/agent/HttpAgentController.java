1d40fb6081dc35052ce4de62eef2cc9b9a4fe2d0
peach_mr@sina.com <peach_mr@sina.com>
2017-04-27 14:37:02 +0800

    更改版本

--- src/main/java/com/onbet/httpagent/agent/HttpAgentController.java
+++ src/main/java/com/onbet/httpagent/agent/HttpAgentController.java
@@ -2,7 +2,7 @@ package com.onbet.httpagent.agent;
 
 import com.onbet.httpagent.utils.HttpClientApi;
 import com.onbet.httpagent.utils.HttpParam;
-import org.springframework.util.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +15,16 @@ import java.util.Map;
 @RequestMapping
 public class HttpAgentController {
     private HttpClientApi api=new HttpClientApi();
+    public static final String WHITESPACE = " ";
+
+
     @RequestMapping("/http")
     public String httpAgent(String url,Map<String,Object> param,String charset){
         if(StringUtils.isEmpty(url)){
             return "";
+        }
+        if (StringUtils.contains(url, WHITESPACE)) {
+            url = url.replaceAll(WHITESPACE, "%20");
         }
         if(StringUtils.isEmpty(charset)){
             charset="utf-8";
